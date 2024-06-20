ff791ff0269051d6d2ede11a8b8ef44e78ccdcd1
DeltaStruct <DeltaStruct47@gmail.com>
2024-06-20 11:20:53 +0000

    commit

--- draw.hpp
+++ draw.hpp
@@ -3,6 +3,20 @@ using namespace std;
 
 class draw_canvas {
   // canvasを使用する
-  draw_canvas() {
+  int height,width;
+  EM_JS(int,get_window_height,(),{
+    return window.innerHeight;
+  });
+  EM_JS(int,get_window_height,(),{
+    return window.innerHeight;
+  });
+  draw_canvas() : height(get_window_height()), width(get_window_width()) {
+    EM_ASM("
+      var canvas = document.createElement('canvas');
+      canvas.id = 'canvas';
+      canvas.height = window.innerHeight;
+      canvas.width = window.innerWidth;
+      document.body.appendChild(canvas);
+    ");
   }
 } draw;
