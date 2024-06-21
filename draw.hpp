d4834d6663ffbf3c984a95f30382c92f425e3538
DeltaStruct <DeltaStruct47@gmail.com>
2024-06-21 05:02:05 +0000

    z

--- draw.hpp
+++ draw.hpp
@@ -1,22 +1,27 @@
 #include <emscripten.h>
+#include <vector>
 using namespace std;
+
+EM_JS(int,get_window_height,(),{
+  return window.innerHeight;
+});
+EM_JS(int,get_window_width,(),{
+  return window.innerWidth;
+});
 
 class draw_canvas {
   // canvasを使用する
   int height,width;
-  EM_JS(int,get_window_height,(),{
-    return window.innerHeight;
-  });
-  EM_JS(int,get_window_height,(),{
-    return window.innerHeight;
-  });
-  draw_canvas() : height(get_window_height()), width(get_window_width()) {
-    EM_ASM("
+  public:
+  draw_canvas(){
+    height = get_window_height();
+    width = get_window_width();
+    EM_ASM({
       var canvas = document.createElement('canvas');
       canvas.id = 'canvas';
       canvas.height = window.innerHeight;
       canvas.width = window.innerWidth;
       document.body.appendChild(canvas);
-    ");
+    });
   }
 } draw;
