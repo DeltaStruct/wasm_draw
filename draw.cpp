ce2f0ff5f7d9ebf4a8e11d76d9f908e1c476643f
DeltaStruct <DeltaStruct47@gmail.com>
2024-06-21 07:28:30 +0000

    add function

--- draw.cpp
+++ draw.cpp
@@ -1,21 +1,15 @@
 #include <emscripten.h>
+#include <iostream>
 #include <vector>
 using namespace std;
-
-EM_JS(int,get_window_height,(),{
-  return window.innerHeight;
-});
-EM_JS(int,get_window_width,(),{
-  return window.innerWidth;
-});
 
 class draw_canvas {
   // canvasを使用する
   int height,width;
   public:
   draw_canvas(){
-    height = get_window_height();
-    width = get_window_width();
+    height = EM_ASM_INT({ return window.innerHeight; });
+    width = EM_ASM_INT({ return window.innerWidth; });
     EM_ASM({
       var canvas = document.createElement('canvas');
       canvas.id = 'canvas';
@@ -23,9 +17,167 @@ class draw_canvas {
       canvas.width = window.innerWidth;
       document.body.appendChild(canvas);
     });
+    cout << height << ' ' << width << endl;
+  }
+
+  /* Call JavaScript function*/
+  void fillRect(int x,int y,int w,int h){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.fillRect($0,$1,$2,$3);
+    },x,y,w,h);
+  }
+  void strokeRect(int x,int y,int w,int h){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.strokeRect($0,$1,$2,$3);
+    },x,y,w,h);
+  }
+  void clearRect(int x,int y,int w,int h){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.clearRect($0,$1,$2,$3);
+    },x,y,w,h);
+  }
+  void fillText(const char* text,int x,int y){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.fillText(UTF8ToString($0),$1,$2);
+    },text,x,y);
+  }
+  void strokeText(const char* text,int x,int y){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.strokeText(UTF8ToString($0),$1,$2);
+    },text,x,y);
+  }
+  void beginPath(){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.beginPath();
+    });
+  }
+  void closePath(){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.closePath();
+    });
+  }
+  void moveTo(int x,int y){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.moveTo($0,$1);
+    },x,y);
+  }
+  void lineTo(int x,int y){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.lineTo($0,$1);
+    },x,y);
+  }
+  void stroke(){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.stroke();
+    });
+  }
+  void fill(){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.fill();
+    });
+  }
+  void arc(int x,int y,int r,int s,int e){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.arc($0,$1,$2,$3,$4);
+    },x,y,r,s,e);
+  }
+  void arcTo(int x1,int y1,int x2,int y2,int r){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.arcTo($0,$1,$2,$3,$4);
+    },x1,y1,x2,y2,r);
+  }
+  void quadraticCurveTo(int cpx,int cpy,int x,int y){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.quadraticCurveTo($0,$1,$2,$3);
+    },cpx,cpy,x,y);
+  }
+  void bezierCurveTo(int cp1x,int cp1y,int cp2x,int cp2y,int x,int y){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext
+      ctx.bezierCurveTo($0,$1,$2,$3,$4,$5);
+    },cp1x,cp1y,cp2x,cp2y,x,y);
+  }
+  void rect(int x,int y,int w,int h){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.rect($0,$1,$2,$3);
+    },x,y,w,h);
+  }
+  void clip(){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.clip();
+    });
+  }
+  void isPointInPath(int x,int y){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.isPointInPath($0,$1);
+    },x,y);
+  }
+  void isPointInStroke(int x,int y){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.isPointInStroke($0,$1);
+    },x,y);
+  }
+  void rotate(int angle){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.rotate($0);
+    },angle);
+  }
+  void scale(int x,int y){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.scale($0,$1);
+    },x,y);
+  }
+  void translate(int x,int y){
+    EM_ASM({
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.translate($0,$1);
+    },x,y);
   }
 } draw;
 
 int main(){
+  draw.fillRect(0,0,0,0); draw.strokeRect(0,0,0,0);
   return 0;
 }
